41f09607a34380f24d9ca5346cfcf35b7218911a
Dharshan-Dandy <dharshandandy@gmail.com>
2024-04-08 21:26:28 +0530

    Constructor....

---
+++ constructor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "🚀 CONFIGURING YOUR WORKSPACE... Please grab a snack and enjoy the show! 🍿"
+
+# Make Necessary Directories...
+
+mkdir -p ~/.bin
+
+# Move the Files that are related to the Tab Completion & Setups Support...
+
+mv quriks teleport ~/.bin
+
+# Append the .bashrc Setups commands to .bashrc...
+
+cat workspace_config.txt >> ~/.bashrc
+
+echo "Now Setups are Finished..."
